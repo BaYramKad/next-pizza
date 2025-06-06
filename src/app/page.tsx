8bcf137c9c60c80bf5902a0ff5621712ea93ec46
bayram.bayranbekov <bayram.bayranbekov@citeck.ru>
2025-06-06 13:27:23 +0300

    add search ingredients

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-import { Button } from '@/components/ui/button';
+import { Container, Filters, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
-  return <Button variant='outline'>213</Button>;
+  return (
+    <>
+      <Container className="mt-13">
+        <Title text="Все пиццы" size="md" className="font-extrabold mb-4" />
+      </Container>
+      <TopBar />
+      <Container className="pb-14 mt-6">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+          <div>Список товаров</div>
+        </div>
+      </Container>
+    </>
+  );
 }
---
+++ src/components/shared/index.ts
@@ -0,0 +1,9 @@
+export { Container } from './container';
+export { Header } from './header';
+export { Categories } from './categories';
+export { SortPopup } from './sort-popup';
+export { TopBar } from './top-bar';
+export { Title } from './title';
+export { FilterCheckbox } from './filter-checkbox';
+export { Filters } from './filters';
+export { CheckboxFiltersGroup } from './checkbox-filters-group';
